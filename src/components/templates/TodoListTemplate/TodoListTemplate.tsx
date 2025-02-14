a1514c1801d1e5745cd0c5b7d1f59b12c7e4d6f5
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-02-15 02:23:12 +0900

    search form

--- src/components/templates/TodoListTemplate/TodoListTemplate.tsx
+++ src/components/templates/TodoListTemplate/TodoListTemplate.tsx
@@ -1,25 +1,50 @@
+import { useMemo } from "react";
+import { useForm, Controller } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { BaseLayout, TodoList } from "../../organisms";
 import { InputForm } from "../../atoms";
 import { useTodoContext } from "../../../hooks/useTodoContext";
-import { useTodoListTemplate } from "./useTodoListTemplate";
 import styles from "./style.module.css";
+
+const searchSchema = z.object({
+  keyword: z.string(),
+});
 
 export const TodoListTemplate = () => {
   // コンテキストから状態とロジックを呼び出してコンポーネントにあてがう
   const { originTodoList, deleteTodo } = useTodoContext();
 
-  const { searchKeyword, showTodoList, handleChangeSearchKeyword } =
-    useTodoListTemplate({ originTodoList });
+  const { control, watch } = useForm({
+    resolver: zodResolver(searchSchema),
+    defaultValues: { keyword: "" },
+  });
+  const searchKeyword = watch("keyword");
+
+  /* 表示用TodoList */
+  const showTodoList = useMemo(() => {
+    const regexp = new RegExp("^" + searchKeyword, "i");
+    return originTodoList.filter((todo) => {
+      // 検索キーワードに部分一致したTodoだけを一覧表示する
+      return todo.title.match(regexp);
+    });
+    // useMemoの第二引数([originTodoList, searchKeyword])に依存して処理が実行される
+    // originTodoListとsearchKeywordの値が変更される度にfilterの検索処理が実行
+    // ただし結果が前回と同じならキャッシュを返却し処理は実行されない(無駄な処理を省いている)
+    // 詳しくはuseMemoを調べてください。
+  }, [originTodoList, searchKeyword]);
 
   return (
     <BaseLayout title={"TodoList"}>
       <div className={styles.container}>
         {/* Todo検索フォームエリア */}
         <div className={styles.area}>
-          <InputForm
-            value={searchKeyword}
-            placeholder={"Search Keyword"}
-            onChange={handleChangeSearchKeyword}
+          <Controller
+            name="keyword"
+            render={({ field }) => (
+              <InputForm placeholder={"Search Keyword"} {...field} />
+            )}
+            control={control}
           />
         </div>
         {/* Todoリスト一覧表示 */}
